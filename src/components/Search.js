e3055739e1eb4d23f79d33bc3e16435ddead6a99
Clovis antunes <clovissantanna@gmail.com>
2023-04-17 17:03:48 -0300

    alterando display

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,8 +3,74 @@ import './styles/search.css';
 function Search () {
     return (
     <div className="Search_container">
-        tem um formulario aqui
+        <section>
+      <div class="list_container">
+        <div class="list_class">
+          <div class="style_form">
+            <select class="ls-label col-md-8" id="option_card">
+              <option value="agent">Alugar</option>
+              <option value="agent">Compra</option>
+            </select>
+          </div>
 
+          <div class="style_form">
+            <select class="ls-label col-md-8" id="option_card">
+              <option value="agent">Tipo de imovel</option>
+              <option value="imovel">Casa</option>
+              <option value="imovel">Apartamento</option>
+              <option value="imovel">Geminiado</option>
+              <option value="imovel">Terreno</option>
+              <option value="imovel">Chacara</option>
+              <option value="imovel">Predio industrial</option>
+              <option value="imovel">Sala comercial</option>
+              <option value="imovel">Sobrado</option>
+            </select>
+          </div>
+          <div class="style_form">
+            <select class="ls-label col-md-8" id="option_card">
+              <option value="agent">Cidade</option>
+              <option value="imovel">Sapiranga</option>
+              <option value="imovel">Campo Bom</option>
+              <option value="imovel">Ararica</option>
+              <option value="imovel">Nova Hartz</option>
+              <option value="imovel">Novo hamburgo</option>
+              <option value="imovel">Parobe</option>
+              <option value="imovel">Porto alegre</option>
+              <option value="imovel">Canoas</option>
+            </select>
+          </div>
+          <div class="style_form">
+            <select class="ls-label col-md-8" id="option_card">
+              <option value="imovel">Vagas para carro</option>
+              <option value="imovel">Sim</option>
+              <option value="imovel">Não</option>
+            </select>
+          </div>
+          <div class="style_form">
+            <select class="ls-label col-md-8" id="option_card">
+              <option value="agent">Dormitórios</option>
+              <option value="agent">1+ Dormitórios</option>
+              <option value="agent">2+ Dormitórios</option>
+              <option value="agent">3+ Dormitórios</option>
+              <option value="agent">4+ Dormitórios</option>
+              <option value="agent">5+ Dormitórios</option>
+            </select>
+          </div>
+          <div class="style_code">
+            <input
+              type="text"
+              id="code"
+              placeholder="Digite o codigo do imovel"
+            />
+          </div>
+          <div class="search_container">
+            <a href="#">
+              <button type="submit" class="search_card">🔍Buscar</button>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
     </div>
     )
 }
